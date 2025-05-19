040a3547c508390debebfd6b317a6e60289f7223
Jerry Huang <17321421572@qq.com>
2025-05-19 15:42:58 +0800

    添加了一个创建新的card layout的布局，以及几个menu按键，add_menu用来打开PageMainActivity页面。有几个问题还没解决，一个是setting_menu的方法，还有监听通知打开新页面的方法。

---
+++ app/src/main/java/org/wit/killbill/BackGroundService/BackGroundService.kt
@@ -0,0 +1,25 @@
+package org.wit.killbill.BackGroundService
+
+import android.app.Service.START_STICKY
+import android.content.Intent
+import android.os.IBinder
+import android.app.Service
+
+class BackGroundService: Service() {
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        intent?.let{
+
+        }
+        // 执行后台任务
+        return START_STICKY
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        // 清理资源
+    }
+}
